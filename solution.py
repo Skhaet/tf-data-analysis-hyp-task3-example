a525c2102769950f53ea993f887c4dd3d58e0f52
Skhaet <62569156+Skhaet@users.noreply.github.com>
2023-04-13 21:27:14 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
+import random 
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 270620880 # Ваш chat ID, не меняйте название переменной
 
 def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    return bool(random.getrandbits(1)) # Ваш ответ, True или False
